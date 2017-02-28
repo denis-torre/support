4caef86994e890145ef03a8b4d78a7b9389a4661
denis-torre <denis.torre@mssm.edu>
2017-02-28 17:05:55 -0500

    Initial Upload

---
+++ Support.R
@@ -0,0 +1,449 @@
+#################################################################
+#################################################################
+############### Pipeline Support
+#################################################################
+#################################################################
+
+#############################################
+########## 1. Load libraries
+#############################################
+##### 1. General support #####
+
+##### 2. Other libraries #####
+
+
+#######################################################
+#######################################################
+########## S1. PCA Analysis
+#######################################################
+#######################################################
+
+#############################################
+########## 1. runPCA
+#############################################
+
+runPCA <- function(expressionDataframe)
+{
+	# Calculate PCA results
+	pcaResults <- prcomp(t(expressionDataframe), scale=FALSE, center=TRUE)
+
+	# Get variance explained
+	varExplained <- (pcaResults$sdev)^2/sum(pcaResults$sdev^2)
+
+	# Add names
+	names(varExplained) <- colnames(pcaResults$x)
+
+	# Get labels
+	varExplainedLabels <- sapply(names(varExplained), function(x) paste0(x, ' (', round(varExplained[x]*100, digits=1),'% var. explained)'))
+
+	# Return results
+	return(list(x=pcaResults$x, var=varExplained, varLabels=varExplainedLabels))
+}
+
+#############################################
+########## 2. getPcaDataframe
+#############################################
+
+getPcaDataframe <- function(expressionDataframe, PCs=c('PC1', 'PC2', 'PC3'), sampleColumn=FALSE)
+{
+	# Calculate PCA results
+	pca <- runPCA(expressionDataframe)
+
+	# Get matrix
+	pcaMatrix <- pca$x
+
+	# Add variance explained
+	pcaMatrix <- rbind(varExplained=pca$varLabels[colnames(pcaMatrix)], pcaMatrix)
+
+	# Get subset
+	pcaMatrix <- as.data.frame(pcaMatrix[,PCs])
+
+	# Add sample ID
+	if (sampleColumn != FALSE) {
+		pcaMatrix <- as.data.frame(cbind(sample_id=rownames(pcaMatrix), pcaMatrix))
+	}
+
+	# Return matrix
+	return(pcaMatrix)
+}
+
+#######################################################
+#######################################################
+########## S2. Dataframe Labeling
+#######################################################
+#######################################################
+
+#############################################
+########## 1. getLabels
+#############################################
+
+getLegend <- function(ids, labels, colors=c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999')) {
+
+	# Get label-color matching
+	label2color <- setNames(colors[1:length(unique(labels))], as.character(unique(labels)))
+
+	# Get id-label matching
+	id2label <- setNames(as.character(labels), ids)
+
+	# Get color vector
+	colorVector <- sapply(ids, function(x) label2color[[id2label[[x]]]])
+
+	# Return
+	return(list(colors=colorVector, legend=label2color))
+}
+
+#############################################
+########## 2. getDesign
+#############################################
+
+getDesign <- function(annotationDataframe, column) {
+
+	# Create new dataframe
+	designDataframeMelt <- data.frame(rows=rownames(annotationDataframe),
+	                                  columns=annotationDataframe[,column],
+	                                  values=1)
+
+	# Cast
+	designDataframe <- dcast(rows ~ columns, data=designDataframeMelt, value.var='values', fill=0)
+
+	# Fix rownames
+	rownames(designDataframe) <- designDataframe$rows
+	designDataframe$rows <- NULL
+
+	# Convert to matrix
+	designMatrix <- as.matrix(designDataframe)
+
+	# Return matrix
+	return(designMatrix)
+}
+
+#######################################################
+#######################################################
+########## S3. Expression Normalization
+#######################################################
+#######################################################
+
+#############################################
+########## 1. runVST
+#############################################
+
+runVST <- function(rawcountDataframe) {
+
+	# Load library
+	require(DESeq2)
+
+	# Convert to matrix
+	rawcountMatrix <- as.matrix(rawcountDataframe)
+
+	# Perform VST normalization
+	vstMatrix <- varianceStabilizingTransformation(rawcountMatrix)
+
+	# Return matrix
+	return(vstMatrix)
+}
+
+#############################################
+########## 2. runVoom
+#############################################
+
+runVoom <- function(rawcountDataframe, annotationDataframe=FALSE) {
+
+	# Load library
+	require(limma)
+
+	# Convert to matrix
+	rawcountMatrix <- as.matrix(rawcountDataframe)
+
+	# Get design, if specified
+	if (annotationDataframe != FALSE) {
+		designMatrix <- getDesign(annotationDataframe, colnames(annotationDataframe))
+	} else {
+		designMatrix <- NULL
+	}
+
+	# Perform VST normalization
+	voomMatrix <- voom(rawcountMatrix, design=designMatrix)$E
+
+	# Return matrix
+	return(voomMatrix)
+}
+
+#############################################
+########## 3. runSizeFactorNormalization
+#############################################
+
+runSizeFactorNormalization <- function(rawcountDataframe, log=TRUE) {
+
+	# Load library
+	require(DESeq2)
+
+	# Convert to matrix
+	rawcountMatrix <- as.matrix(rawcountDataframe)
+
+	# Estimate size factors
+	sizeFactors <- estimateSizeFactorsForMatrix(rawcountMatrix)
+
+	# Perform VST normalization
+	normalizedCountMatrix <- t(t(rawcountMatrix)/sizeFactors)
+
+	# Log transform, if specified
+	if (log) {
+		normalizedCountMatrix <- log10(normalizedCountMatrix + 1)
+	}
+
+	# Return matrix
+	return(normalizedCountMatrix)
+}
+
+#############################################
+########## 4. Quantile Normalization
+#############################################
+
+runQuantileNormalization <- function(expressionDataframe) {
+
+	# Load library
+	require(preprocessCore)
+
+	# Convert to matrix
+	expressionMatrix <- as.matrix(expressionDataframe)
+
+	# Normalize
+	normalizedExpressionMatrix <- normalize.quantiles(expressionMatrix)
+
+	# Fix dimension names
+	rownames(normalizedExpressionMatrix) <- rownames(expressionMatrix)
+	colnames(normalizedExpressionMatrix) <- colnames(expressionMatrix)
+
+	# Return matrix
+	return(normalizedExpressionMatrix)
+}
+
+#############################################
+########## 5. Batch Effect Removal
+#############################################
+
+runComBat <- function(expressionDataframe, annotationDataframe, covariateFormula=NULL, batchColumn='batch') {
+	
+    # Load library
+    require(sva)
+
+    # Get batch
+    batch <- annotationDataframe[,batchColumn]
+
+    # Get covariate, if specified
+    if (!is.null(covariateFormula)) {
+        covariateDesign <- model.matrix(as.formula(covariateFormula), data=annotationDataframe)
+    } else {
+        covariateDesign <- NULL
+    }
+    
+    # Run Combat
+    combatDataframe <- ComBat(dat=expressionDataframe, batch=batch, mod=covariateDesign, par.prior=TRUE, prior.plots=FALSE)
+
+    # Return dataframe
+    return(combatDataframe)
+}
+
+#######################################################
+#######################################################
+########## S4. Differential Expression
+#######################################################
+#######################################################
+
+#############################################
+########## 1. runCharacteristicDirection
+#############################################
+
+runCharacteristicDirectionOld <- function(expressionDataframe, experimentColumns, controlColumns, minVariance=0, r=1) {
+
+	# Load libraries
+	source('/Users/denis/Documents/Projects/scripts/libraries/geoDE/chdir.R')
+	source('/Users/denis/Documents/Projects/scripts/libraries/geoDE/nipals.R')
+
+	# Convert to R character
+	experimentColumns <- as.character(experimentColumns)
+	controlColumns <- as.character(controlColumns)
+
+	# Get dataframes
+	experimentDataframe <- expressionDataframe[,experimentColumns]
+	controlDataframe <- expressionDataframe[,controlColumns]
+
+	# Get variance
+	experimentGeneVariance <- apply(experimentDataframe, 1, var)
+	controlGeneVariance <- apply(controlDataframe, 1, var)
+
+	# Get common genes
+	commonGenes <- intersect(names(experimentGeneVariance)[experimentGeneVariance > minVariance], names(controlGeneVariance)[controlGeneVariance > minVariance])
+
+	# Filter
+	experimentDataframeFiltered <- experimentDataframe[commonGenes,]
+	controlDataframeFiltered <- controlDataframe[commonGenes,]
+
+	# Run Characteristic Direction
+	cdResults <- chdir(controlDataframeFiltered, experimentDataframeFiltered, commonGenes)
+
+	# Convert to dataframe
+	cdDataframe <- data.frame(CD=cdResults)
+
+	# Return results
+	return(cdDataframe)
+}
+
+##########
+##### New Function - Updated filter.  Should return more complete, slightly different results
+##########
+
+
+runCharacteristicDirection <- function(expressionDataframe, experimentColumns, controlColumns, constantThreshold=1e-5) {
+
+	# Load libraries
+	source('/Users/denis/Documents/Projects/scripts/libraries/geoDE/chdir.R')
+	source('/Users/denis/Documents/Projects/scripts/libraries/geoDE/nipals.R')
+
+	# Convert to R character
+	experimentColumns <- as.character(experimentColumns)
+	controlColumns <- as.character(controlColumns)
+
+	# Get dataframes
+	experimentDataframe <- expressionDataframe[,experimentColumns]
+	controlDataframe <- expressionDataframe[,controlColumns]
+
+	# Get variable genes
+	experimentVariableGenes <- names(which(diag(var(t(experimentDataframe))) > constantThreshold))
+	controlVariableGenes <- names(which(diag(var(t(controlDataframe))) > constantThreshold))
+
+	# Get common genes
+	commonGenes <- intersect(experimentVariableGenes, controlVariableGenes)
+
+	# Filter dataframes
+	experimentDataframeFiltered <- experimentDataframe[commonGenes,]
+	controlDataframeFiltered <- controlDataframe[commonGenes,]
+
+	# Run Characteristic Direction
+	cdResults <- chdir(controlDataframeFiltered, experimentDataframeFiltered, commonGenes)
+
+	# Convert to dataframe
+	cdDataframe <- data.frame(CD=cdResults)
+
+	# Return results
+	return(cdDataframe)
+}
+
+
+
+#######################################################
+#######################################################
+########## S5. Gene ID Conversion
+#######################################################
+#######################################################
+
+#############################################
+########## 1. convertIDs
+#############################################
+
+convertIDs <- function(genes, orgDb='org.Hs.eg.db', originalIdType='ENTREZID', newIdType='SYMBOL') {
+
+	# Load libraries
+	require(AnnotationDbi)
+	require(orgDb, character.only=TRUE)
+
+	# Convert to character
+	genes <- as.character(genes)
+
+	# Remove NAs and get unique
+	genes <- unique(genes[!is.na(genes)])
+
+	# Get conversion dataframe
+	conversionDataframe <- select(get(orgDb), keys=genes, keytype=originalIdType, columns=newIdType)
+
+	# Return dataframe
+	return(conversionDataframe)
+}
+
+
+#######################################################
+#######################################################
+########## S6. Statistical Analysis
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Matrix correlation
+#############################################
+
+correlateMatrices <- function(matrix1, matrix2, method='pearson', use='everything') {
+	return(cor(t(matrix1), matrix2, method=method, use=use))
+}
+
+#######################################################
+#######################################################
+########## S7. L1000CDS2
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Count small molecules
+#############################################
+
+getL1000CDS2Counts <- function(signatureDataframe, aggravate, group=FALSE, plot=TRUE, nDrugs=10, col=c('#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'), main='', ylab='', order=FALSE, legendTitle=NULL) {
+    
+    # Filter
+    signatureDataframeFiltered <- signatureDataframe[signatureDataframe$aggravate == aggravate,]
+    
+    # If group
+    if (group != FALSE) {
+        
+        # Count
+        drugCounts <- table(signatureDataframeFiltered$pert_desc, signatureDataframeFiltered[,group])
+        
+        # Sort
+        drugCounts <- drugCounts[order(apply(drugCounts, 1, sum), decreasing=TRUE),]
+        
+        # Get total counts
+        totalDrugCounts <- sort(apply(drugCounts, 1, sum), decreasing=TRUE)
+        
+        # Filter
+        drugCounts <- drugCounts[totalDrugCounts > 0,]
+        
+        # Plot
+        if (plot) {
+            if (order == FALSE) {
+                order <- 1:ncol(drugCounts)
+            }
+            topDrugs <- names(rev(totalDrugCounts[1:nDrugs]))
+            barplot(t(drugCounts[topDrugs, order]), horiz=TRUE, las=2, col=col,
+                    xlab='Times in top 50 signatures', ylab=ylab, main=main)
+            legend('bottomright', fill=col, legend=colnames(drugCounts)[order], title=legendTitle)
+        }
+        
+    } else {
+        
+        # Count
+        drugCounts <- table(signatureDataframeFiltered$pert_desc)
+                
+        # Sort
+        drugCounts <- sort(drugCounts, decreasing=TRUE)
+        
+        # Filter
+        drugCounts <- drugCounts[drugCounts > 0]
+        
+        # Plot
+        if (plot) {
+            barplot(rev(drugCounts[1:nDrugs]), horiz=TRUE, las=2, col=col,
+                    xlab='Times in top 50 signatures', ylab=ylab, main=main)
+        }
+    }
+    # Return counts
+    return(drugCounts)
+}
+
+#######################################################
+#######################################################
+########## S. 
+#######################################################
+#######################################################
+
+#############################################
+########## . 
+#############################################
+
---
+++ Support.py
@@ -0,0 +1,307 @@
+#################################################################
+#################################################################
+############### Pipeline Support
+#################################################################
+#################################################################
+
+#############################################
+########## 1. Load libraries
+#############################################
+##### 1. General support #####
+import os, sklearn.decomposition, requests, json
+import rpy2.robjects as robjects
+import numpy as np
+import pandas as pd
+from plotly.graph_objs import Scatter3d, Layout
+
+##### 2. Other libraries #####
+
+
+#######################################################
+#######################################################
+########## S1. R Connection
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Get R source
+#############################################
+
+def setupR(rSource):
+
+	# Setup R
+	r = robjects.r
+
+	# Add source
+	r.source(rSource)
+
+	# Return object
+	return r
+
+#######################################################
+#######################################################
+########## S2. Plotting
+#######################################################
+#######################################################
+
+#############################################
+########## 1. 3D Plotly Scatter
+#############################################
+
+def plot3dScatter(dataframe, categoricalColumn, varLabelDict, annotationColumn=False, title='', PCs=['PC1', 'PC2', 'PC3'], colors=['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'], width=900, height=600, size=5):
+
+	# Define empty plot
+	p = []
+
+	# Get unique categories
+	uniqueCategories = list(set(dataframe[categoricalColumn]))
+
+	# Get dict with unique categories
+	categoricalExpressionDict = {x:dataframe[dataframe[categoricalColumn] == x] for x in uniqueCategories}
+
+	# Loop through categories
+	for i in range(len(uniqueCategories)):
+
+	    # Get category
+	    category = uniqueCategories[i]
+
+	    # Get plot dataframe
+	    plotDataframe = categoricalExpressionDict[category]
+	    
+	    # Get annotation, if specified
+	    annotation = plotDataframe[annotationColumn] if annotationColumn else ''
+
+	    # Append trace
+	    p.append(
+	        Scatter3d(
+	            x = plotDataframe[PCs[0]],
+	            y = plotDataframe[PCs[1]],
+	            z = plotDataframe[PCs[2]],
+	            mode='markers',
+	            text=annotation,
+	            name=category,
+	            marker=dict(
+	                size=size,
+	                color=colors[i],
+	                opacity=0.9
+	            ),
+	        )
+	    )
+	    
+	# Add layout
+	layout = Layout(
+	    title=title,
+	    hovermode='closest',
+	    width=width,
+	    height=height,
+	    scene=dict(
+	        xaxis=dict(title=varLabelDict[PCs[0]]),
+	        yaxis=dict(title=varLabelDict[PCs[1]]),
+	        zaxis=dict(title=varLabelDict[PCs[2]]),
+	    ),
+	    margin=dict(
+	        l=50,
+	        r=50,
+	        b=50,
+	        t=50
+	    )
+	)
+
+	# Prepare figure
+	fig = dict(data=p, layout=layout)
+
+	# Return figure
+	return fig
+
+#######################################################
+#######################################################
+########## S3. Enrichr
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Add gene lists 
+#############################################
+
+def addGeneLists(geneset):
+	ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/addList'
+	genes_str = '\n'.join(geneset)
+	payload = {
+	    'list': (None, genes_str),
+	}
+	response = requests.post(ENRICHR_URL, files=payload)
+	if not response.ok:
+	    raise Exception('Error analyzing gene list')
+	data = json.loads(response.text)
+	return data
+
+#############################################
+########## 2. Get enrichment results
+#############################################
+
+def getEnrichmentResults(user_list_id, gene_set_library='GO_Biological_Process_2015'):
+	ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/enrich'
+	query_string = '?userListId=%s&backgroundType=%s'
+	response = requests.get(
+	    ENRICHR_URL + query_string % (user_list_id, gene_set_library)
+	 )
+	if not response.ok:
+	    raise Exception('Error fetching enrichment results')
+
+	data = json.loads(response.text)
+	resultDataframe = pd.DataFrame(data[gene_set_library], columns=['rank', 'term_name', 'pvalue', 'zscore', 'combined_score', 'overlapping_genes', 'FDR', 'old_pvalue', 'old_FDR'])
+	resultDataframe = resultDataframe.loc[:,['term_name','zscore','combined_score','FDR']]
+	return resultDataframe
+
+#############################################
+########## 3. Get and add gene lists 
+#############################################
+
+def uploadToEnrichr(cdDataframe, column, nGenes=500):
+
+	# Get sorted genes
+	sortedGenesAscending = cdDataframe.sort_values(column, ascending=True).index.tolist()
+
+	# Get genesets
+	genesets = {'downregulated': sortedGenesAscending[:nGenes], 'upregulated': sortedGenesAscending[-nGenes:]}
+
+	# Upload and get links
+	genesetLinks = {x: addGeneLists(genesets[x]) for x in genesets.keys()}
+
+	# Add URLs
+	for key in genesetLinks.keys():
+	    genesetLinks[key]['URL'] = 'http://amp.pharm.mssm.edu/Enrichr/enrich?dataset='+genesetLinks[key]['shortId']
+
+    # Convert to dataframe
+	enrichrLinkDataframe = pd.DataFrame(genesetLinks).T.reset_index().rename(columns={'index': 'geneset'})
+
+	# Return links
+	return enrichrLinkDataframe
+
+#######################################################
+#######################################################
+########## S4. L1000CDS2
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Run L1000CDS2
+#############################################
+
+def runL1000CDS2(cdDataframe, column, aggravate=False):
+    # Set data
+    data = {"genes": cdDataframe.index.tolist(), "vals":cdDataframe[column].tolist()}
+    data['genes'] = [x.upper() for x in data['genes']]
+    
+    # Set configuration
+    config = {"aggravate":aggravate, "searchMethod":"CD", "share":True, "combination":False, "db-version":"latest"}
+    payload = {"data":data,"config":config}
+    headers = {'content-type':'application/json'}
+    
+    # Perform request
+    r = requests.post('http://amp.pharm.mssm.edu/L1000CDS2/query',data=json.dumps(payload),headers=headers)
+    resCD= r.json()
+    
+    # Add URL
+    resCD['URL'] = 'http://amp.pharm.mssm.edu/L1000CDS2/#/result/' + resCD['shareId']
+    
+    # Return result
+    return resCD
+
+#############################################
+########## 2. Get L1000CDS2 results
+#############################################
+
+def getL1000CDS2Results(cdDataframe, column):
+
+	# Define result dataframe and list
+	resultSignatureDataframe = pd.DataFrame()
+	linkList = []
+
+	# Loop through aggravate
+	for aggravate in [True, False]:
+	    
+	    # Run analysis
+	    resCD = runL1000CDS2(cdDataframe, column, aggravate)
+	    
+	    # Get signature dataframe
+	    signatureDataframe = pd.DataFrame(resCD['topMeta']).drop('overlap', axis=1).replace('-666', np.nan)
+	    
+	    # Add aggravate column
+	    signatureDataframe['aggravate'] = aggravate
+	    
+	    # Concatenate
+	    resultSignatureDataframe = pd.concat([resultSignatureDataframe, signatureDataframe])
+	    
+	    # Add link
+	    linkList.append({'URL': resCD['URL'], 'aggravate': aggravate})
+	    
+	# Convert link list to dataframe
+	linkDataframe = pd.DataFrame(linkList)
+
+	# Create result dict
+	resultDict = {'signatures': resultSignatureDataframe, 'links': linkDataframe}
+
+	# Return dictionary
+	return resultDict
+
+#######################################################
+#######################################################
+########## S5. Statistical Analysis
+#######################################################
+#######################################################
+
+#############################################
+########## 1. PCA
+#############################################
+
+def runPCA(expressionDataframe, n_components=3):
+    
+    # Initialize PCA object
+    pcaResults = sklearn.decomposition.PCA(n_components=n_components)
+    
+    # Run PCA
+    pcaResults.fit(expressionDataframe)
+    
+    # Get PC dataframe
+    pcaDataframe = pd.DataFrame(pcaResults.components_).T
+
+    # Fix dimension names
+    pcaDataframe.columns = ['PC'+str(x+1) % locals() for x in pcaDataframe.columns]
+    pcaDataframe.index = expressionDataframe.columns
+    
+    # Get variance explained
+    varExplainedDict = {'PC'+str(i+1): 'PC'+str(i+1)+' ('+str(round(pcaResults.explained_variance_[i], ndigits=1))+'% var. explained)'for i in range(len(pcaResults.explained_variance_))}
+
+    # Return result
+    return {'PCs': pcaDataframe, 'varExplained': varExplainedDict}
+
+#############################################
+########## 2. Plot 3D PCA
+#############################################
+
+def plot3DPCA(expressionDataframe, annotationDataframe, categoricalColumn, annotationColumn=False, title='PCA Analysis'):
+
+	# Run PCA
+	pcaResults = runPCA(expressionDataframe)
+
+	# Create plot dataframe
+	plotDataframe = pcaResults['PCs'].merge(annotationDataframe, left_index=True, right_index=True)
+
+	# Create figure
+	fig = plot3dScatter(plotDataframe, categoricalColumn, pcaResults['varExplained'], annotationColumn=annotationColumn, title=title)
+
+	# Return fig
+	return fig
+
+
+#######################################################
+#######################################################
+########## S. 
+#######################################################
+#######################################################
+
+#############################################
+########## . 
+#############################################
+
